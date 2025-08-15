f5fbd7c1d451089eec5f0679d467484772621fc1
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-08-16 06:47:01 +0900

    class discord

--- server/index.php
+++ server/index.php
@@ -23,6 +23,47 @@ if($_SERVER['REQUEST_METHOD']=='POST'&&substr(strtolower($_SERVER['CONTENT_TYPE'
 	}
 }
 
+class discord {
+    private $access_token;
+    function __construct ($options=[
+        'access_token'=>null,
+    ]) {
+        $this->access_token = $options['access_token'];
+        if (!isset($this->access_token) || strlen($this->access_token)==0) {
+            error_log('['.__LINE__.'] ['.$_SERVER['REMOTE_ADDR'].'] '. 'Unable the Class ' . get_class($this) . ' Initialize.');
+            exit(1);
+        }
+    }
+    function getCurrentUser () {
+        /* 
+        * name: getCurrentUser
+        * return: objective-array
+        **/
+        $api_endpoint = 'https://discordapp.com/api/users/@me';
+        $api_customHeader = [
+            'Authorization: Bearer ' . $this->access_token,
+        ];
+        error_log('['.__LINE__.'] ['.$_SERVER['REMOTE_ADDR'].'] '. json_encode([
+            $api_endpoint,
+            $api_customHeader,
+            $_SERVER['REMOTE_ADDR'],
+        ], JSON_UNESCAPED_SLASHES));
+        
+        $curl_req = curl_init();
+        curl_setopt($curl_req, CURLOPT_URL, $api_endpoint);
+        curl_setopt($curl_req, CURLOPT_CUSTOMREQUEST, 'GET');
+        curl_setopt($curl_req, CURLOPT_HTTPHEADER, $api_customHeader);
+        curl_setopt($curl_req, CURLOPT_FRESH_CONNECT, TRUE);
+        curl_setopt($curl_req, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($curl_req, CURLOPT_FOLLOWLOCATION, TRUE);
+
+        $curl_res=curl_exec($curl_req);
+        $curl_res=json_decode($curl_res, TRUE);
+
+        return $curl_res; /* objective-array **/
+    }
+}
+
 echo json_encode([
     'iat' => time(),
     'access_token' => $_POST['access_token'],
